a16154eee6b207b5ad65f339459db752e9d75faa
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-09-24 13:15:13 +0700

    Text Widget

---
+++ lib/basic_widgets/text_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Nama saya Aqueena Regita Hapsari, sedang belajar Pemrograman Mobile",
+      style: TextStyle(color: Color.fromARGB(255, 255, 46, 147), fontSize: 14),
+      textAlign: TextAlign.center);
+  }
+}
